3862113c81ad3970d8fa92aaa225bb30e4661cf9
root <root@LUS-1H85292DN8.localdomain>
2025-11-20 17:06:36 -0500

    Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'katex/dist/katex.min.css'
+import 'katex/dist/katex.css'
 import App from './App.jsx'
 import { BookmarkProvider } from './contexts/BookmarkContext'
 import ErrorBoundary from './components/ErrorBoundary'
