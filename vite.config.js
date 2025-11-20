9825332faccb71656fca14ccfa3fd75891c87918
root <root@LUS-1H85292DN8.localdomain>
2025-11-20 17:31:02 -0500

    finally

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',
+  base: '/ai-training/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
