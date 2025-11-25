73888245cc21257c225ec75de5f2209b5b2770c2
root <root@LUS-1H85292DN8.localdomain>
2025-11-25 09:38:55 -0500

    Revert "Change base path to / for root domain deployment"
    This reverts commit 636209f98e334faf82a32a15b5823b1c01fac68b.

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',
+  base: '/ai-training/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
